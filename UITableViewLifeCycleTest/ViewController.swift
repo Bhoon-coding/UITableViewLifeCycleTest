d9d5901f5299a53e0ac93f4b7d030192fa11ba86
roy-lee2 <roy.lee2@kakaocorp.com>
2024-12-07 15:36:42 +0900

    Initial Commit

---
+++ UITableViewLifeCycleTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewLifeCycleTest
+//
+//  Created by 이병훈 on 12/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
